d93adfcf58ee76d8b6f9b186f9138d1233d87f61
RenetaYordanova <118059302+RenetaYordanova@users.noreply.github.com>
2024-01-15 12:05:09 +0200

    Add a task for Exercise

---
+++ Inheritance/P02HierarchicalInheritance/Cat.java
@@ -0,0 +1,8 @@
+package P02HierarchicalInheritance;
+
+public class Cat extends Animal{
+    public void meo(){
+        System.out.println("meowing...");
+    }
+
+}
---
+++ Inheritance/P02HierarchicalInheritance/Main.java
@@ -0,0 +1,15 @@
+package P02HierarchicalInheritance;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Dog dog=new Dog();
+        dog.bark();
+        dog.eat();
+
+        Cat cat=new Cat();
+        cat.meo();
+        cat.eat();
+
+    }
+}
